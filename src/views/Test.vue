8da1a1362a67ce4739d8e21af5f2c1096b8657b6
AkwGaniu <lmd4sure@gmail.com>
2020-12-02 15:07:29 +0100

    quiz page layout done

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="test-view-container">
+    <h1 class="title">LASU FIRST SEMESTER MOCK EXAMINATION</h1>
     <div class="upper-section">
       <div class="student-details">
         <p class="name">Sodipo Uthman Oluwaseto</p>
         <p class="matric">170591939</p>
       </div>
+      <h1>LASU FIRST SEMESTER MOCK EXAMINATION</h1>
       <div class="timer">
-        <p>30:00</p>
+        <p><b>Time left: </b> 30:00</p>
       </div>
     </div>
     <div class="lower-section">
-      <div class="question-list">
+      <div class="question-tracker">
         <span
           v-for="question in questions"
           :key="question"
@@ -18,8 +20,37 @@
           {{ question }}
         </span>
       </div>
-      <div class="showQuestion">
-        
+      <div class="show-question">
+        <p class="question-track">
+          <b>Question: {{ currentQuestion }}</b>
+        </p>
+        <div class="questions">
+          <p>
+            Phasellus nec interdum dui, sed suscipit ligula. Nam at dui dui. Proin quis massa lacinia, venenatis eros in, laoreet nulla. Etiam laoreet, justo nec facilisis malesuada, magna velit ultrices eros, nec rhoncus sapien augue et sem. Nunc viverra tincidunt auctor. Quisque vel leo ornare, tincidunt turpis ac, faucibus tellus. Etiam et nibh vulputate, molestie diam sed, ultricies nunc. Cras in suscipit sem.
+          </p>
+        </div>
+        <div class="answers">
+          <b-form-group class="left">
+            <b-form-radio-group
+              id="radio-group-1"
+              v-model="selected"
+              :options="options1"
+              name="radio-options2"
+            ></b-form-radio-group>
+          </b-form-group>
+          <b-form-group  class="right">
+            <b-form-radio-group
+              id="radio-group-2"
+              v-model="selected"
+              :options="options2"
+              name="radio-options2"
+            ></b-form-radio-group>
+          </b-form-group>
+        </div>
+        <div class="question-navigator">
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
       </div>
     </div>
   </div>
@@ -28,7 +59,17 @@
 export default {
   data () {
     return {
-      questions: [1, 2, 3, 4, 5, 6, 7, 7, 8, 6, 66, 5, 6]
+      currentQuestion: 1,
+      questions: [1, 2, 3, 4, 5, 6, 7, 7, 8, 6, 66, 5, 6],
+      selected: '',
+      options1: [
+        { text: 'Computational Science (CSC 345)', value: 'Add' },
+        { text: 'Computational Science (CSC 345)', value: 'Blhljlw' }
+      ],
+      options2: [
+        { text: 'Computational Science ', value: 'frggglr' },
+        { text: 'Computational Science ', value: 'Bhkggkgkjhjh' }
+      ]
     }
   }
 }
--- src/views/TestInstruction.vue
+++ src/views/TestInstruction.vue
@@ -1,35 +1,29 @@
 <template>
   <div class="test-instruction-view-container">
-    <div class="nav-head">
-      <div class="logo-holder">
-        <div class="logo">
-          <router-link to="/dashboard">
-            <img src="../assets/BTA11.png" alt="BTA LOGO">
-          </router-link>        </div>
-      </div>
-      <div class="salutation">
-        <p>Good Morning, <span>Ganiu</span></p>
-      </div>
-    </div>
-    <div class="mobile-nav">
-      <div class="logo-holder">
-        <div class="logo">
-          <router-link to="/dashboard">
-            <img src="../assets/BTA11.png" alt="BTA LOGO">
-          </router-link>
+    <h1>Basic Tutor Academy Mock Examination</h1>
+      <div class="sections student-profile">
+        <div class="user-top">
+            <p class="name">
+            <b>Course Title: </b>
+            <span>Computational analysis</span>
+          </p>
+          <p class="matric">
+            <b>Course Code: </b>
+            <span>CSC 419</span>
+          </p>
+        </div>
+        <div class="user-bottom">
+          <p class="dept">
+            <b>Course Unit: </b>
+            <span> 3 </span>
+          </p>
+          <p class="level">
+            <b>Exam Duration: </b>
+
+            <span> 30 Minutes </span>
+          </p>
         </div>
       </div>
-      <div class="toggle-nav">
-        <span>
-          <b-icon
-            v-show="!showMobileNav"
-            icon="list"
-            class="nav-icon"
-            @click="toggleMobileNav"
-          ></b-icon>
-        </span>
-      </div>
-    </div>
     <div class="main-content">
       <p class="instruction">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras semper sagittis nibh in laoreet. Pellentesque porta turpis non metus vestibulum, tempus commodo ipsum dignissim. Donec convallis eleifend sapien, non facilisis metus. Praesent et cursus lorem. Quisque at dui sit amet nulla facilisis aliquet. Etiam euismod imperdiet augue, ut rhoncus sem pellentesque eget. Suspendisse est ipsum, faucibus eget dolor eget, ultricies lacinia ligula. Sed lacinia tristique dui, semper faucibus mauris varius non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc vitae massa imperdiet, condimentum quam ac, imperdiet mi. Proin vitae tellus dictum, maximus est sed, accumsan est. Sed ultrices diam ut nisl euismod, non fringilla lectus placerat. Morbi placerat sapien vel semper tempus. Etiam ut ipsum quis purus rhoncus tincidunt sed eu nisl. Nam faucibus, est volutpat dictum dictum, nulla ligula eleifend risus, ac consectetur justo eros et orci.
