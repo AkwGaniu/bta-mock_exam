e1ffce2b124438fde22aeba51ac178310ca40e2c
AkwGaniu <lmd4sure@gmail.com>
2021-04-17 10:31:24 +0100

    base url updated to live server

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,7 @@
 export default {
   user: Object,
-  // baseUrl: 'https://bta-backend.herokuapp.com/',
-  baseUrl: 'http://localhost:8000/',
+  baseUrl: 'https://bta-backend.herokuapp.com/',
+  // baseUrl: 'http://localhost:8000/',
   pending: false,
   isLoggedIn: !!localStorage.getItem('bta_user_token'),
   isAdminLoggedIn: !!localStorage.getItem('bta_admin_token'),
